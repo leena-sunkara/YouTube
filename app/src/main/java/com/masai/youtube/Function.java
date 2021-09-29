1850b8fc6911fa243b0f44d37568a915468c605d
Vijay-Dahiya <vijaydahiyano.1@gmail.com>
2021-09-29 09:28:00 +0530

    vijay

---
+++ app/src/main/java/com/masai/youtube/Function.java
@@ -0,0 +1,96 @@
+package com.masai.youtube;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class Function extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageView home;
+    private ImageView shorts;
+    private ImageView upload;
+    private ImageView sub;
+    private ImageView library;
+    private FragmentManager fragmentManager;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_function);
+
+
+        fragmentManager = getSupportFragmentManager();
+        initViews();
+
+    }
+
+    private void initViews() {
+        home= findViewById(R.id.ivHome);
+        shorts = findViewById(R.id.ivShorts);
+        upload = findViewById(R.id.ivUpload);
+        sub = findViewById(R.id.ivSub);
+        library = findViewById(R.id.ivLibrary);
+        addHome();
+        home.setOnClickListener(this);
+        shorts.setOnClickListener(this);
+        upload.setOnClickListener(this);
+        sub.setOnClickListener(this);
+        library.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+        int id= v.getId();
+        switch (id){
+            case R.id.ivHome:
+                addHomeAgain();
+                break;
+            case R.id.ivShorts:
+                Intent intent = new Intent(Function.this, ShortsActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.ivSub:
+                addSubscribe();
+                break;
+            case R.id.ivLibrary:
+                addLibrary();
+                break;
+
+
+        }
+
+    }
+    private void addHome(){
+        FragmentTransaction transaction= fragmentManager.beginTransaction();
+        HomeFagment homeFagment=new HomeFagment();
+        transaction.add(R.id.flContainer,homeFagment,"fragmentA").commit();
+    }
+    private void addHomeAgain(){
+        FragmentTransaction transaction= fragmentManager.beginTransaction();
+        HomeFagment homeFagment=new HomeFagment();
+        transaction.add(R.id.flContainer,homeFagment,"fragmentA").addToBackStack("aaa").commit();
+    }
+
+
+    private void addSubscribe(){
+        FragmentTransaction transaction= fragmentManager.beginTransaction();
+        SubscribeFragment subscribeFragment=new SubscribeFragment();
+        transaction.add(R.id.flContainer,subscribeFragment,"fragmentB").addToBackStack("aaa").commit();
+    }
+    private void addLibrary(){
+        FragmentTransaction transaction= fragmentManager.beginTransaction();
+        LibraryFragment libraryFragment=new LibraryFragment();
+        transaction.add(R.id.flContainer,libraryFragment,"fragmentB").addToBackStack("aaa").commit();
+    }
+
+}
---
+++ app/src/main/java/com/masai/youtube/ShortsActivity.java
@@ -0,0 +1,14 @@
+package com.masai.youtube;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShortsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shorts);
+    }
+}

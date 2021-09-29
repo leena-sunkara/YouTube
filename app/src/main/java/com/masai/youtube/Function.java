5fdc28a2ae496972079c50af934ed123dc8d1304
Leena Sunkara <leenasunkara444@gmail.com>
2021-09-29 18:38:48 +0530

    Sign in Authentication

--- app/src/main/java/com/masai/youtube/Function.java
+++ app/src/main/java/com/masai/youtube/Function.java
@@ -1,13 +1,13 @@
 package com.masai.youtube;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class Function extends AppCompatActivity implements View.OnClickListener {
 
@@ -18,21 +18,17 @@ public class Function extends AppCompatActivity implements View.OnClickListener 
     private ImageView library;
     private FragmentManager fragmentManager;
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_function);
 
-
         fragmentManager = getSupportFragmentManager();
         initViews();
-
     }
 
     private void initViews() {
-        home= findViewById(R.id.ivHome);
+        home = findViewById(R.id.ivHome);
         shorts = findViewById(R.id.ivShorts);
         upload = findViewById(R.id.ivUpload);
         sub = findViewById(R.id.ivSub);
@@ -43,15 +39,13 @@ public class Function extends AppCompatActivity implements View.OnClickListener 
         upload.setOnClickListener(this);
         sub.setOnClickListener(this);
         library.setOnClickListener(this);
-
     }
-
 
     @Override
     public void onClick(View v) {
 
-        int id= v.getId();
-        switch (id){
+        int id = v.getId();
+        switch (id) {
             case R.id.ivHome:
                 addHomeAgain();
                 break;
@@ -65,32 +59,30 @@ public class Function extends AppCompatActivity implements View.OnClickListener 
             case R.id.ivLibrary:
                 addLibrary();
                 break;
-
-
         }
-
-    }
-    private void addHome(){
-        FragmentTransaction transaction= fragmentManager.beginTransaction();
-        HomeFagment homeFagment=new HomeFagment();
-        transaction.add(R.id.flContainer,homeFagment,"fragmentA").commit();
-    }
-    private void addHomeAgain(){
-        FragmentTransaction transaction= fragmentManager.beginTransaction();
-        HomeFagment homeFagment=new HomeFagment();
-        transaction.add(R.id.flContainer,homeFagment,"fragmentA").addToBackStack("aaa").commit();
     }
 
-
-    private void addSubscribe(){
-        FragmentTransaction transaction= fragmentManager.beginTransaction();
-        SubscribeFragment subscribeFragment=new SubscribeFragment();
-        transaction.add(R.id.flContainer,subscribeFragment,"fragmentB").addToBackStack("aaa").commit();
-    }
-    private void addLibrary(){
-        FragmentTransaction transaction= fragmentManager.beginTransaction();
-        LibraryFragment libraryFragment=new LibraryFragment();
-        transaction.add(R.id.flContainer,libraryFragment,"fragmentB").addToBackStack("aaa").commit();
+    private void addHome() {
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        HomeFagment homeFagment = new HomeFagment();
+        transaction.add(R.id.flContainer, homeFagment, "fragmentA").commit();
     }
 
+    private void addHomeAgain() {
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        HomeFagment homeFagment = new HomeFagment();
+        transaction.add(R.id.flContainer, homeFagment, "fragmentA").addToBackStack("aaa").commit();
+    }
+
+    private void addSubscribe() {
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        SubscribeFragment subscribeFragment = new SubscribeFragment();
+        transaction.add(R.id.flContainer, subscribeFragment, "fragmentB").addToBackStack("aaa").commit();
+    }
+
+    private void addLibrary() {
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        LibraryFragment libraryFragment = new LibraryFragment();
+        transaction.add(R.id.flContainer, libraryFragment, "fragmentB").addToBackStack("aaa").commit();
+    }
 }
